8ed04cb054dbea269e3a7f588e5bb158472143a7
virtualramblas <guglielmo.iozzia@gmail.com>
2023-03-10 12:13:30 +0000

    Added the requirements file.

---
+++ export_codegen_to_onnx.py
@@ -0,0 +1,58 @@
+import argparse
+import os
+import sys
+
+file_dir = os.path.dirname(os.path.abspath(__file__))
+codegen_dir = os.path.join(file_dir, "CodeGen")
+sys.path.append(codegen_dir)
+from jaxformer.hf.sample import (
+    create_model,
+    create_custom_gpt2_tokenizer,
+    set_seed,
+)
+
+from fastgpt import (
+    generate_onnx_representation,
+    quantize,
+    test_onnx_inference,
+    test_torch_inference,
+)
+
+def export(checkpoint="checkpoints/codegen-350M-mono"):
+    pad_token = 50256
+    model = create_model(ckpt=checkpoint, fp16=False)
+    model.eval()
+    tokenizer = create_custom_gpt2_tokenizer()
+    tokenizer.padding_side = "left"
+    tokenizer.pad_token = pad_token
+
+    # Settings to try out the pre-trained PyTorch model
+    rng_seed = 42
+    rng_deterministic = True
+    p = 0.95
+    t = 0.2
+    max_length = 128
+    batch_size = 1
+    context = "def hello_world():"
+    set_seed(rng_seed, deterministic=rng_deterministic)
+
+    # Test the model
+    test_torch_inference(model)
+
+    # Convert the original model to the ONNX format
+    onnx_path = generate_onnx_representation(model)
+    model_path = checkpoint
+    onnx_path = os.path.join(model_path, "onnx/model.onnx")
+    test_onnx_inference(onnx_path, model.config)
+
+    # Quantize the ONNX model
+    quantized_onnx_path = quantize(onnx_path)
+    test_onnx_inference(quantized_onnx_path, model.config)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model_path", 
+                        type=str, 
+                        help="The path of the pre-trained CodeGen model to export.")
+    args = parser.parse_args()
+    export(args.model_path)
